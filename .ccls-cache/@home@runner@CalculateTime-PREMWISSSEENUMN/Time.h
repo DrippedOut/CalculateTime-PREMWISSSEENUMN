5e9c1bc4942f0347c47b9d5406aa792a5a698c72
DrippedOut <86663132+DrippedOut@users.noreply.github.com>
2023-05-28 13:31:32 +0000

    Commit my files

--- .ccls-cache/@home@runner@CalculateTime-PREMWISSSEENUMN/Time.h
+++ .ccls-cache/@home@runner@CalculateTime-PREMWISSSEENUMN/Time.h
@@ -1,11 +1,11 @@
 
-// function 1
+//function 1
 void getTime(int &h,int &m){
   cout << "Input hour & min:";
   cin >> h >> m;
 }
 
-// function 2
+//function 2
 int subtract(int h1, int m1, int h2,int m2){
   // return hour
   int h = h2 - h1;
@@ -14,9 +14,9 @@ int subtract(int h1, int m1, int h2,int m2){
   return h;
 } 
 
-// function 3
+//function 3
 int subtract(int m1,int m2) {
   // return minute
   if (m2<m1) return m2+60-m1;
-  else return m2-m1;
+  else return m2-m1; 
 }
